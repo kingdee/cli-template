abe9079d479089c3d1e9967e5810e1c2ba62bf72
kimjin788 <kimj7486@gmail.com>
2026-01-23 16:49:20 +0800

    refactor: 移除示例组件中关于logo路径的注释

--- app/kwc/exampleComponent/exampleComponent.js
+++ app/kwc/exampleComponent/exampleComponent.js
@@ -3,10 +3,6 @@ import { KingdeeElement, track } from '@kdcloudjs/kwc';
 export default class ExampleComponent extends KingdeeElement {
   @track count = 0;
 
-  // Use a getter or simple property. 
-  // In dev mode, assets should be copied to dist.
-  // We'll assume the logo is available at the root or relative path.
-  // If copied to dist root:
   logoUrl = 'logo.png';
 
   handleClick() {
